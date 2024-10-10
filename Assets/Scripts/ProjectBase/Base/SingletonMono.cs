1c171bd834af9c58acc91e3d9f8b0056dd327868
NFSW <1262510555@qq.com>
2024-10-10 17:56:30 +0800

    给普通Mono单例添加使用说明

--- Assets/Scripts/ProjectBase/Base/SingletonMono.cs
+++ Assets/Scripts/ProjectBase/Base/SingletonMono.cs
@@ -1,4 +1,27 @@
-﻿using System.Collections;
+﻿// 使用场景:(可挂载--可访问场景节点的函数方法，可全局访问)
+// 比如一个场景中有多个MonoBehaviour组件，需要实现单例模式，那么可以直接继承这个类，然后到场景中挂载这个脚本，最后在脚本中调用GetInstance方法即可。
+// 
+// 
+// 使用案例:
+// 
+// 假设有个需要作为单例的组件叫`MyMono`，那么可以这样写：
+// public class MyMono : SingletonAutoMono<MyMono>
+// {
+//     //此处添加自己的函数逻辑, 假设这里有一个方法叫`DoSomething()`
+// }
+// 
+// 然后在需要的地方调用 `MyMono.GetInstance()` 即可获得单例的`MyMono`对象
+// public class Test : MonoBehaviour
+// {
+//     void Start()
+//     {
+//         MyMono.GetInstance().DoSomething();  // 通过静态方法访问
+//         MyMono.Instacne.DoSomething();       // 通过静态属性访问
+//     }
+// }
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
