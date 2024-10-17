3db7257e8e47c7ec82750e91f704f3402f80daf3
Ikun <Ikun@123.com>
2024-10-17 12:57:30 +0800

    清除单例

--- Assets/Scripts/Player/PlayerData/PlayerData_Temp.cs
+++ Assets/Scripts/Player/PlayerData/PlayerData_Temp.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class PlayerData_Temp : MonoBehaviour
 {
     /// <summary>
-    /// 单例实例
-    /// </summary>
-    public static PlayerData_Temp Instance { get; private set; }
-
-    /// <summary>
     /// 玩家血量
     /// </summary>
     public int health = 100;
@@ -18,23 +13,6 @@ public class PlayerData_Temp : MonoBehaviour
     /// 玩家弹药
     /// </summary>
     public int ammo = 4;
-
-    void Awake()
-    {
-        /// <summary>
-        /// 确保只有一个实例存在
-        /// </summary>
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject); // 保持对象在场景切换时不被销毁
-        }
-        else
-        {
-            Destroy(gameObject); // 如果已经有一个实例，销毁新的
-        }
-    }
-
     // Start is called before the first frame update
     void Start()
     {
