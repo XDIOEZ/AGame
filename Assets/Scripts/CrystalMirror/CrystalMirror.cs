a90c4e048d5e373af53ea0a4ebaf8f10e1f44065
NFSW <1262510555@qq.com>
2024-10-20 15:15:37 +0800

    水晶镜初步准备

---
+++ Assets/Scripts/CrystalMirror/CrystalMirror.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalMirror : MonoBehaviour
+{
+    public string[] acceptedTags; // 可接受的对象标签
+    public float speedMultiplier = 1.0f; // 速度变化倍数
+}
