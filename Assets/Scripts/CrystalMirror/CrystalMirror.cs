d92cbb360d0f4b2b28488c485b7e419f848d7d39
NFSW <1262510555@qq.com>
2024-10-23 17:40:35 +0800

    传送反射镜修复

--- Assets/Scripts/CrystalMirror/CrystalMirror.cs
+++ Assets/Scripts/CrystalMirror/CrystalMirror.cs
@@ -6,4 +6,16 @@ public class CrystalMirror : MonoBehaviour
 {
     public string[] acceptedTags; // 可接受的对象标签
     public float speedMultiplier = 1.0f; // 速度变化倍数
+
+    void Start()
+    {
+        // 添加事件监听器
+        EventCenter.Instance.AddEventListener(
+            "Teleport_Light_Bullet",
+            () =>
+            {
+                MusicMgr.Instance.PlaySound("OnHitMirror", false);
+            }
+        );
+    }
 }
