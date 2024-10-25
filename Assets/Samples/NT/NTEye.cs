6e539ac2aa801b5beedd6ccaff9cc43d408167b5
NFSW <1262510555@qq.com>
2024-10-25 16:55:13 +0800

    fixed

--- Assets/Samples/NT/NTEye.cs
+++ Assets/Samples/NT/NTEye.cs
@@ -4,47 +4,37 @@ using UnityEngine;
 
 public class NTEye : MonoBehaviour
 {
-    public float moveSpeed = 2f; // 移动速度
-    public float smoothTime = 0.2f; // 缓动时间
-    private Vector3 currentVelocity = Vector3.zero; // 当前速度
+    public float moveSpeed = 8f; // 移动速度
     private Rigidbody2D rb; // Rigidbody2D 组件
 
-    private void Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        Vector3 moveDirection = Vector3.zero;
+        Vector2 moveDirection = Vector2.zero;
 
         if (Input.GetKey(KeyCode.W))
         {
-            moveDirection += Vector3.up;
+            moveDirection += Vector2.up;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            moveDirection += Vector3.down;
+            moveDirection += Vector2.down;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            moveDirection += Vector3.left;
+            moveDirection += Vector2.left;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            moveDirection += Vector3.right;
+            moveDirection += Vector2.right;
         }
 
         moveDirection.Normalize();
-        Vector3 targetPosition = moveDirection * moveSpeed;
-
-        rb.MovePosition(
-            Vector3.SmoothDamp(
-                transform.position,
-                transform.position + targetPosition,
-                ref currentVelocity,
-                smoothTime
-            )
-        );
+        
+        rb.velocity = moveDirection * moveSpeed;
     }
 }
