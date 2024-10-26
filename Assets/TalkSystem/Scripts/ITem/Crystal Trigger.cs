cbde4b8d3560019042249d7950e84f147c0c9f4d
ZTl1280 <1970382689qq.com>
2024-10-26 12:09:45 +0800

    电晶石over

--- Assets/TalkSystem/Scripts/ITem/Crystal Trigger.cs
+++ Assets/TalkSystem/Scripts/ITem/Crystal Trigger.cs
@@ -9,9 +9,9 @@ public class CrystalTrigger : MonoBehaviour
     public bool Left;
     public bool Up;
     public bool Down;
+    public GameObject[] brother;
 
-
-    public float speed = 5.0f;
+    public float speed;
     public float timer;
     private float timers;
 
@@ -25,77 +25,92 @@ public class CrystalTrigger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        timers = timer;
+        timers= 0f;
         ParentRb2d = GetComponentInParent<Rigidbody2D>();
-        Parents =GetComponentInParent<Crystalreflection>();
+        Parents = GetComponentInParent<Crystalreflection>();
     }
 
     private void Update()
     {
-        timers-=Time.deltaTime;
-        if(timer>0)
+        timers-= Time.deltaTime;
+        if (timers>0)
         {
             Vector2 moveDirection = new Vector2(horizontalMove, verticalMove);
             moveDirection *= speed;
-
+            Debug.Log("001");
             ParentRb2d.velocity += moveDirection;
-
-            Debug.Log(ParentRb2d.velocity);
-
-           
-        }
-
-    }
-
-    // Update is called once per frame
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            if (Right)
-            { Parents.ReBoundPlayer(1);
-                timers = timer;
-                horizontalMove = -1.0f;
-            }
-            else if (Left)
-            { Parents.ReBoundPlayer(2);
-                timers = timer;
-                horizontalMove = 1.0f;
-            }
-            else if (Up)
-            { Parents.ReBoundPlayer(3);
-                timers = timer;
-                verticalMove = -1.0f;
-            }
-            else 
-            { Parents.ReBoundPlayer(4);
-                timers = timer;
-                verticalMove = 1.0f;
+            for (int i = 0; i < brother.Length; i++)
+            {
+                brother[i].SetActive(false);
             }
         }
-        if (collision.gameObject.CompareTag("LittlePlayer"))
+        else
         {
-            if (Right)
-            { Parents.ReBoundPlayer(1);
-                timers = timer;
-                horizontalMove = -1.0f;
-            }
-            else if (Left)
-            { Parents.ReBoundPlayer(2);
-                timers = timer;
-                horizontalMove = 1.0f;
-            }
-            else if (Up)
-            { Parents.ReBoundPlayer(3);
-                timers = timer;
-                verticalMove = -1.0f;
-            }
-            else
-            { Parents.ReBoundPlayer(4);
-                timers = timer;
-                verticalMove = 1.0f;
+            ParentRb2d.velocity = Vector2.zero;
+            for (int i = 0; i < brother.Length; i++)
+            {
+                brother[i].SetActive(true);
             }
         }
     }
-}
+
+        // Update is called once per frame
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                if (Parents.openKey || Parents.Player.isLittle)
+                {
+                    if (Right)
+                    {
+                    Parents.ReBoundPlayer(1);
+                    timers = timer;
+                    horizontalMove = -1.0f;
+                    }
+                    else if (Left)
+                    {
+                    Parents.ReBoundPlayer(2);
+                    timers = timer;
+                    horizontalMove = 1.0f;
+                    }
+                    else if (Up)
+                    {
+                    Parents.ReBoundPlayer(3);
+                    timers = timer;
+                    verticalMove = -1.0f;
+                    }
+                    else
+                    {
+                    Parents.ReBoundPlayer(4);
+                    timers = timer;
+                    verticalMove = 1.0f;
+                    }
+                }
+
+            }
+            if (collision.gameObject.CompareTag("LittlePlayer"))
+            {
+                if (Right)
+                { Parents.ReBoundPlayer(1);
+                timers = timer;
+                horizontalMove = -1.0f;
+                }
+                else if (Left)
+                { Parents.ReBoundPlayer(2);
+                timers = timer;
+                horizontalMove = 1.0f;
+                }
+                else if (Up)
+                { Parents.ReBoundPlayer(3);
+                timers = timer;
+                verticalMove = -1.0f;
+                }
+                else
+                { Parents.ReBoundPlayer(4);
+                timers = timer;
+                verticalMove = 1.0f;
+                }
+            }
+        }
+    }
