0c87c6f18b691a1b52131a10f11a95fcbe765817
西贝子游 <jiaziyou8@qq.com>
2024-10-27 12:58:53 +0800

    no message

--- Assets/Scripts/Objects/Camera/CameraController.cs
+++ Assets/Scripts/Objects/Camera/CameraController.cs
@@ -6,7 +6,7 @@ public class CameraController : MonoBehaviour
 {
     public Transform  target;   //玩家位置
 
-    public Transform BackGround/*,foreGround*/;  //背景
+    public Transform BackGround,foreGround;  //背景
 
     private Vector2 lastPos;//相机最后位置
 
@@ -21,11 +21,11 @@ public class CameraController : MonoBehaviour
     {
         transform.position = new Vector3(target.position.x, target.position.y, target.position.z);//相机跟随玩家位置
 
-        Vector2 moveAmount = new Vector2(transform.position.x - lastPos.x, transform.position.y-lastPos.y);
+        Vector2 moveAmount = new Vector2(transform.position.x - lastPos.x, transform.position.y - lastPos.y);
 
-        BackGround.position += new Vector3(moveAmount.x, moveAmount.y/2, 0f);
+        BackGround.position += new Vector3(moveAmount.x, moveAmount.y, 0f);
 
-        //foreGround.position += new Vector3(-moveAmount.x * 0.5f, -moveAmount.y * 0.5f, 0f);
+        foreGround.position += new Vector3(-moveAmount.x * 0.5f, -moveAmount.y * 0.5f, 0f);
 
         lastPos = transform.position;
 
